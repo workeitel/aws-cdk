837dac46f4aefb110ec26f13dabd5468d5d9d7c6
Matthew Pirocchi <pirocchi@amazon.com>
2018-08-15 12:38:34 -0700

    Create empty solution and project.

---
+++ packages/aws-cdk/lib/init-templates/app/dotnet/src/HelloCdk/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloCdk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
